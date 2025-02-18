3cc781b97053c79c868be771fb340514f1e7f8d7
Hubert <jaroszhubert12@gmail.com>
2025-02-18 20:14:13 +0100

    dsadadsda
    adsad

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-// Efekt parallax - Ruch tła podczas przewijania strony
-window.addEventListener('scroll', function() {
-    const header = document.querySelector('header');
-    let offset = window.pageYOffset;
-    header.style.backgroundPosition = 'center ' + offset * 0.5 + 'px';
+// Obsługa otwierania i zamykania menu
+const menuToggle = document.getElementById('menu-toggle');
+const sidebar = document.getElementById('sidebar');
+
+// Funkcja do otwierania/zamykania menu
+menuToggle.addEventListener('click', () => {
+    sidebar.classList.toggle('open'); // Dodajemy lub usuwamy klasę 'open'
 });
